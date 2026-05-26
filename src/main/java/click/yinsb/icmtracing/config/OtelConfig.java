05c79acff9b44c82fa67dee4c684e01c10be3abd
uniquejava <uniquejava@gmail.com>
2026-05-26 20:52:35 +0800

    cleanup

--- src/main/java/click/yinsb/icmtracing/config/OtelConfig.java
+++ src/main/java/click/yinsb/icmtracing/config/OtelConfig.java
@@ -20,7 +20,7 @@ public class OtelConfig {
     @Bean
     OpenTelemetry openTelemetry() {
         Resource resource = Resource.getDefault()
-                .merge(Resource.create(Attributes.of(AttributeKey.stringKey("service.name"), "multi-trace-groups")));
+                .merge(Resource.create(Attributes.of(AttributeKey.stringKey("service.name"), "icm-tracing")));
 
         SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
                 .setResource(resource)
